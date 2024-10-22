89337abeb2c80c51229ea858adcc10fbfc3a7570
JackR2882 <sgjrive@liverpool.ac.uk>
2024-10-22 11:13:03 +0000

    Basic packet analysis for IP packets

--- sniffer.cpp
+++ sniffer.cpp
@@ -12,17 +12,164 @@
 #include <iostream>
 #include <pcap.h>
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/if_ether.h>
+#include <net/ethernet.h>
+#include <netinet/ether.h>
 
 
-void handle_packet(u_char *useless,
+#ifndef ETHER_HDRLEN
+#define ETHER_HDRLEN 14
+#endif
+
+u_int16_t handle_ethernet(u_char *args,
+                        const struct pcap_pkthdr* pkthdr,
+                        const u_char* packet);
+u_char* handle_IP(u_char *args,
+                const struct pcap_pkthdr* pkthdr,
+                const u_char* packet);
+
+
+/* structure of an ethernet header - sourced from tcpdump */
+struct my_ip {
+	u_int8_t	ip_vhl;		/* header length, version */
+#define IP_V(ip)	(((ip)->ip_vhl & 0xf0) >> 4)
+#define IP_HL(ip)	((ip)->ip_vhl & 0x0f)
+	u_int8_t	ip_tos;		/* type of service */
+	u_int16_t	ip_len;		/* total length */
+	u_int16_t	ip_id;		/* identification */
+	u_int16_t	ip_off;		/* fragment offset field */
+#define	IP_DF 0x4000			/* dont fragment flag */
+#define	IP_MF 0x2000			/* more fragments flag */
+#define	IP_OFFMASK 0x1fff		/* mask for fragmenting bits */
+	u_int8_t	ip_ttl;		/* time to live */
+	u_int8_t	ip_p;		/* protocol */
+	u_int16_t	ip_sum;		/* checksum */
+	struct	in_addr ip_src,ip_dst;	/* source and dest address */
+};
+
+
+
+
+void handle_packet(u_char *args,
                 const struct pcap_pkthdr* pkthdr,
                 const u_char* packet)
 {
+    
+    
+    u_int16_t type = handle_ethernet(args, pkthdr, packet);
+
+    if(type== ETHERTYPE_IP)
+    {
+        handle_IP(args, pkthdr, packet);
+    }
+
     static int count = 1;
     std::cout << "Packet " << count << ": ";
     std::cout << "Successfully captured a packet of length: " << pkthdr[count-1].len << '\n';
     count++;
+
 }
+
+
+
+u_char* handle_IP(u_char *args, const struct pcap_pkthdr* pkthdr, const u_char* packet)
+{
+    const struct my_ip* ip;
+    //u_int length = pkthdrif->len;
+    u_int length = ip->ip_len;
+    u_int hlen,off,version;
+    int i;
+
+    int len;
+
+    ip = (struct my_ip*)(packet + sizeof(struct ether_header));
+    length -= sizeof(struct ether_header);
+
+    /* check length is valid */
+    if (length < sizeof(struct my_ip))
+    {
+        std::cout << "Truncated IP " << length << '\n';
+        return(NULL); 
+    }
+
+    len = ntohs(ip->ip_len);
+    hlen = IP_HL(ip);
+    version = IP_V(ip);
+
+    if (version != 4)
+    {
+        std::cout << "Unknown version " << version << '\n';
+    }
+
+    if (hlen < 5)
+    {
+        std::cout << "Bad header length " << hlen << '\n';
+    }
+
+    if (length < len)
+    {
+        std::cout << "Truncated IP " << len-length << " bytes missing!\n";
+    }
+
+    /* check to see if we have the first fragment */
+    off = ntohs(ip->ip_off);
+    if((off & 0x1fff) == 0)
+    {
+        std::cout << "IP: " << inet_ntoa(ip->ip_src) << '\n';
+        std::cout << "Header length: " << hlen << '\n';
+        std::cout << "Version: " << version << '\n';
+        std::cout << "Offset? " << off << '\n';
+    }
+
+    return(NULL);    
+
+}
+
+
+
+u_int16_t handle_ethernet(u_char *args, const struct pcap_pkthdr* pkthdr, const u_char* packet)
+{
+    u_int caplen = pkthdr->caplen;
+    u_int length = pkthdr->len;
+    struct ether_header *eptr;
+    u_short ether_type;
+
+    if (caplen < ETHER_HDRLEN)
+    {
+        std::cout << "Packet length less than ethernet header length!\n";
+        return(-1);
+    }
+
+    eptr = (struct ether_header *) packet;
+    ether_type = ntohs(eptr->ether_type);
+
+    std::cout << "ETH: " << ether_ntoa((struct ether_addr*)eptr->ether_shost) << '\n';
+    
+
+    if (ether_type == ETHERTYPE_IP)
+    {
+        std::cout << "(IP)\n";
+    } else if (ether_type == ETHERTYPE_ARP)
+    {
+        std::cout << "(ARP)\n";
+    } else if (eptr->ether_type == ETHERTYPE_REVARP)
+    {
+        std::cout << "(RARP)\n";
+    } else
+    {
+        std::cout << "(?)\n";
+    }
+
+    return(ether_type);
+
+
+}
+
+
+
 
 
 int main()
@@ -53,8 +200,8 @@ int main()
 
 
     /* grab a packet */
-    packet = pcap_next(handle, &header);
-    std::cout << "Successfully captured a packet of length: " << header.len << '\n';
+    //packet = pcap_next(handle, &header);
+    //std::cout << "Successfully captured a packet of length: " << header.len << '\n';
     /* close the session */
     //pcap_close(handle);
 
